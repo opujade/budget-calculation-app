5dac67e20b64617bb28cdb4f72f563a3a53decbc
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-05-03 16:00:41 +0200

    webdatafixed

--- src/data/webOptionsData.js
+++ src/data/webOptionsData.js
@@ -3,13 +3,13 @@ const webOptionsData = [
     name: 'pàgines',
     price: 30,
     amount: 0,
-    definition: `Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de ${this.price}€.`,
+    definition: `Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de 30€.`,
   },
   {
     name: 'llenguatges',
     price: 30,
     amount: 0,
-    definition: `Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de ${this.price}€.`,
+    definition: `Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€.`,
   },
 ];
 
