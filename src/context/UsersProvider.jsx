fe83eeb7989d803d23e274499ce4a5783fa62021
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-05-03 17:21:18 +0200

    exercise 7 sort finished

--- src/context/UsersProvider.jsx
+++ src/context/UsersProvider.jsx
@@ -1,23 +1,209 @@
 import { useState, useContext, createContext } from 'react';
 
 const usersContext = createContext();
+const updateUsersContext = createContext();
 const addUsersContext = createContext();
 const newUserContext = createContext();
 const updateNewUserContext = createContext();
 
 export const useUsersContext = () => useContext(usersContext);
+export const useUpdateUsersContext = () => useContext(updateUsersContext);
 export const useAddUsersContext = () => useContext(addUsersContext);
 export const useNewUserContext = () => useContext(newUserContext);
 export const useUpdateNewUserContext = () => useContext(updateNewUserContext);
 
 export const UsersProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([{
+    "nom": "Oriol",
+    "tel": "123123123",
+    "email": "oriol@oriol.com",
+    "serveis": [
+      {
+        "title": "Ads",
+        "description": "Programació d'una web responsive completa",
+        "price": 400,
+        "id": 2
+      }
+    ],
+    "total": 400,
+    "data": 1714747531673,
+  },
+  {
+    "nom": "Pol",
+    "tel": "321321321",
+    "email": "pol@pol.com",
+    "serveis": [],
+    "total": 0,
+    "data": 1714747531669,
+  },
+  {
+    "nom": "Manel",
+    "tel": "222222222",
+    "email": "manel@manel.com",
+    "serveis": [
+      {
+        "title": "Web",
+        "description": "Programació d'una web responsive completa",
+        "price": 500,
+        "id": 3,
+        "options": [
+          {
+            "name": "pàgines",
+            "price": 30,
+            "amount": 5,
+            "definition": "Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de 30€."
+          },
+          {
+            "name": "llenguatges",
+            "price": 30,
+            "amount": 6,
+            "definition": "Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€."
+          }
+        ]
+      }
+    ],
+    "total": 620,
+    "data": 1714747581570,
+  }, {
+    "nom": "Antoni",
+    "tel": "111222333",
+    "email": "antoni@antoni.com",
+    "serveis": [
+      {
+        "title": "Seo",
+        "description": "Programació d'una web responsive completa",
+        "price": 300,
+        "id": 1
+      },
+      {
+        "title": "Ads",
+        "description": "Programació d'una web responsive completa",
+        "price": 400,
+        "id": 2
+      }
+    ],
+    "total": 700,
+    "data": 1714747593832,
+  },
+  {
+    "nom": "Felip",
+    "tel": "333222111",
+    "email": "felip@felip.com",
+    "serveis": [
+      {
+        "title": "Web",
+        "description": "Programació d'una web responsive completa",
+        "price": 500,
+        "id": 3,
+        "options": [
+          {
+            "name": "pàgines",
+            "price": 30,
+            "amount": 5,
+            "definition": "Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de 30€."
+          },
+          {
+            "name": "llenguatges",
+            "price": 30,
+            "amount": 6,
+            "definition": "Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€."
+          }
+        ]
+      },
+      {
+        "title": "Ads",
+        "description": "Programació d'una web responsive completa",
+        "price": 400,
+        "id": 2
+      },
+      {
+        "title": "Seo",
+        "description": "Programació d'una web responsive completa",
+        "price": 300,
+        "id": 1
+      }
+    ],
+    "total": 1380,
+    "data": 1714747610928,
+  },
+  {
+    "nom": "Anselm",
+    "tel": "1212212211",
+    "email": "anselm@anselm.com",
+    "serveis": [
+      {
+        "title": "Web",
+        "description": "Programació d'una web responsive completa",
+        "price": 500,
+        "id": 3,
+        "options": [
+          {
+            "name": "pàgines",
+            "price": 30,
+            "amount": 5,
+            "definition": "Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de 30€."
+          },
+          {
+            "name": "llenguatges",
+            "price": 30,
+            "amount": 6,
+            "definition": "Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€."
+          }
+        ]
+      },
+      {
+        "title": "Ads",
+        "description": "Programació d'una web responsive completa",
+        "price": 400,
+        "id": 2
+      }
+    ],
+    "total": 1080,
+    "data": 1714747627208,
+  },
+  {
+    "nom": "Antonia",
+    "tel": "123456789",
+    "email": "antonia@antonia.com",
+    "serveis": [
+      {
+        "title": "Web",
+        "description": "Programació d'una web responsive completa",
+        "price": 500,
+        "id": 3,
+        "options": [
+          {
+            "name": "pàgines",
+            "price": 30,
+            "amount": 5,
+            "definition": "Afegeix les pàgines que necessitis per a dur a terme el teu projecte. El cost de cada pàgina és de 30€."
+          },
+          {
+            "name": "llenguatges",
+            "price": 30,
+            "amount": 6,
+            "definition": "Afegeix les llenguatges que tindrà el teu projecte. El cost de cada llenguatge és de 30€."
+          }
+        ]
+      },
+      {
+        "title": "Seo",
+        "description": "Programació d'una web responsive completa",
+        "price": 300,
+        "id": 1
+      }
+    ],
+    "total": 1130,
+    "data": 1714747637544,
+  }
+  ]);
   const [newUser, setNewUser] = useState({
     nom: '',
     tel: '',
     email: '',
     serveis: [],
     total: 0,
+    data: '',
   });
 
   const resetNewUser = () => {
@@ -27,6 +213,7 @@ export const UsersProvider = ({ children }) => {
       email: '',
       serveis: [],
       total: 0,
+      data: '',
     });
   };
 
@@ -35,20 +222,29 @@ export const UsersProvider = ({ children }) => {
   };
 
   const addUsers = () => {
-    setUsers([...users, newUser]);
+    let newUserAux = newUser;
+    const data = new Date().getTime();
+    newUserAux.data = data;
+    setUsers([...users, newUserAux]);
     resetNewUser();
   };
+
+  const updateUsers = (usersAux) => {
+    setUsers([...usersAux]);
+  }
 
   return (
     <>
       <usersContext.Provider value={users}>
-        <addUsersContext.Provider value={addUsers}>
-          <newUserContext.Provider value={newUser}>
-            <updateNewUserContext.Provider value={updateNewUser}>
-              {children}
-            </updateNewUserContext.Provider>
-          </newUserContext.Provider>
-        </addUsersContext.Provider>
+        <updateUsersContext.Provider value={updateUsers}>
+          <addUsersContext.Provider value={addUsers}>
+            <newUserContext.Provider value={newUser}>
+              <updateNewUserContext.Provider value={updateNewUser}>
+                {children}
+              </updateNewUserContext.Provider>
+            </newUserContext.Provider>
+          </addUsersContext.Provider>
+        </updateUsersContext.Provider>
       </usersContext.Provider>
     </>
   );
--- src/pages/Prices/Budgets.jsx
+++ src/pages/Prices/Budgets.jsx
@@ -5,10 +5,12 @@ import {
   useNewUserContext,
   useUsersContext,
   useUpdateNewUserContext,
+  useUpdateUsersContext,
 } from '../../context/UsersProvider';
 
 export const Budgets = ({ checkMethods }) => {
   const users = useUsersContext();
+  const updateUsers = useUpdateUsersContext();
   const updateNewUser = useUpdateNewUserContext();
   const newUser = useNewUserContext();
   const addUser = useAddUsersContext();
@@ -48,6 +50,50 @@ export const Budgets = ({ checkMethods }) => {
     addUser();
     checkMethods.unCheckAll();
   };
+
+  const sortUsers = (option) => {
+    let usersAux = users;
+
+    switch (option) {
+      case 'Data': {
+        usersAux.sort((a, b) => {
+          if (a.data < b.data) {
+            return -1;
+          } else if (a.data > b.data) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      }
+      case 'Import': {
+        usersAux.sort((a, b) => {
+          if (a.total < b.total) {
+            return -1;
+          } else if (a.total > b.total) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      }
+      case 'Nom': {
+        usersAux.sort((a, b) => {
+          if (a.nom < b.nom) {
+            return -1;
+          } else if (a.nom > b.nom) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      }
+    }
+    updateUsers(usersAux);
+  }
 
   return (
     <>
@@ -93,6 +139,17 @@ export const Budgets = ({ checkMethods }) => {
         <h3 className="font-bold text-4xl md:mt-10 mt-5 mb-5 md:text-start text-center">
           Pressupostos en curs:
         </h3>
+        <div className='flex justify-end'>
+          <div className="join mx-5 flex">
+            <input className="input input-bordered border-e-0 join-item" placeholder="Buscar" />
+            <div className="join-item border input-bordered border-s-0 px-4 h-12 flex justify-center items-center"><svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512"><path fill="#bababa" d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" /></svg></div>
+          </div>
+          <div className="join">
+            <button onClick={() => sortUsers('Data')} className="btn join-item">Data <svg xmlns="http://www.w3.org/2000/svg" height="14" width="8.75" viewBox="0 0 320 512"><path fill="#000000" d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" /></svg></button>
+            <button onClick={() => sortUsers('Import')} className="btn join-item">Import <svg xmlns="http://www.w3.org/2000/svg" height="14" width="8.75" viewBox="0 0 320 512"><path fill="#000000" d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" /></svg></button>
+            <button onClick={() => sortUsers('Nom')} className="btn join-item">Nom <svg xmlns="http://www.w3.org/2000/svg" height="14" width="8.75" viewBox="0 0 320 512"><path fill="#000000" d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8H32c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8H288c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z" /></svg></button>
+          </div>
+        </div>
         {userBox}
       </div>
     </>
